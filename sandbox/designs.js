f5b403f70c0813cb704bb538058a4cc6e06e3a21
Gerald Bauer <gerald.bauer@gmail.com>
2021-08-13 13:30:28 +0200

    add random

--- sandbox/designs.js
+++ sandbox/designs.js
@@ -46,6 +46,52 @@
                                   }} );
 
 
+  const zombie_design = `
+  ###
+  # zombie (male) - basic - zero attributes
+  #
+  #  colors:
+  #   0 - transparent
+  #   1 - black
+  #
+  #  size: 24x24
+
+  . . . . . . . . . . . . . . . . . . . . . . . .
+  . . . . . . . . . . . . . . . . . . . . . . . .
+  . . . . . . . . . . . . . . . . . . . . . . . .
+  . . . . . . . . . . . . . . . . . . . . . . . .
+  . . . . . . . . . . . . . . . . . . . . . . . .
+  . . . . . . . . @ @ @ @ @ @ @ . . . . . . . . .
+  . . . . . . . @ o o o o o o o @ . . . . . . . .
+  . . . . . . @ o o ~ o o o o o o @ . . . . . . .
+  . . . . . . @ o ~ o o o o o o o @ . . . . . . .
+  . . . . . . @ o o o o o o o o o @ . . . . . . .
+  . . . . . . @ o o o o o o o o o @ . . . . . . .
+  . . . . . . @ o o x x o o o x x @ . . . . . . .
+  . . . . . @ o o o ^ @ o o o ^ @ @ . . . . . . .
+  . . . . . @ o o o x o o o o x o @ . . . . . . .
+  . . . . . @ @ o o o o o o o o o @ . . . . . . .
+  . . . . . . @ o o o o o @ @ o o @ . . . . . . .
+  . . . . . . @ o o o o o o o o o @ . . . . . . .
+  . . . . . . @ o o o o o o o o o @ . . . . . . .
+  . . . . . . @ o o o o @ @ @ o o @ . . . . . . .
+  . . . . . . @ o o o o x o o o o @ . . . . . . .
+  . . . . . . @ o o o o o o o o @ . . . . . . . .
+  . . . . . . @ o o o @ @ @ @ @ . . . . . . . . .
+  . . . . . . @ o o o @ . . . . . . . . . . . . .
+  . . . . . . @ o o o @ . . . . . . . . . . . . .
+`
+
+const zombie = Pixelart.parse( zombie_design, {
+  colors: {
+    '@':  '#000000',       // color 1 - black
+    'x':  '#5e7253',
+    'o':  '#7da269',
+    '^':  '#ff0000',
+    '~':  '#9bbc88',
+  }} );
+
+
   const demon_design = `
   ###
 # demon (male) - basic - zero attributes
@@ -91,6 +137,83 @@
                                 }} );
 
 
+
+const human_design = `
+###
+# human (male) - basic - zero attributes
+#
+#  colors:
+#   0 - transparent
+#   1 - black
+#
+#  size: 24x24
+
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . @ @ @ @ @ @ @ . . . . . . . . .
+. . . . . . . @ ^ ^ ^ ^ ^ ^ ^ @ . . . . . . . .
+. . . . . . @ ^ ^ ~ ^ ^ ^ ^ ^ ^ @ . . . . . . .
+. . . . . . @ ^ ~ ^ ^ ^ ^ ^ ^ ^ @ . . . . . . .
+. . . . . . @ ^ ^ ^ ^ ^ ^ ^ ^ ^ @ . . . . . . .
+. . . . . . @ ^ ^ ^ ^ ^ ^ ^ ^ ^ @ . . . . . . .
+. . . . . . @ ^ ^ x x ^ ^ ^ x x @ . . . . . . .
+. . . . . @ ^ ^ ^ @ o ^ ^ ^ @ o @ . . . . . . .
+. . . . . @ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ @ . . . . . . .
+. . . . . @ @ ^ ^ ^ ^ ^ ^ ^ ^ ^ @ . . . . . . .
+. . . . . . @ ^ ^ ^ ^ ^ @ @ ^ ^ @ . . . . . . .
+. . . . . . @ ^ ^ ^ ^ ^ ^ ^ ^ ^ @ . . . . . . .
+. . . . . . @ ^ ^ ^ ^ ^ ^ ^ ^ ^ @ . . . . . . .
+. . . . . . @ ^ ^ ^ ^ @ @ @ ^ ^ @ . . . . . . .
+. . . . . . @ ^ ^ ^ ^ ^ ^ ^ ^ ^ @ . . . . . . .
+. . . . . . @ ^ ^ ^ ^ ^ ^ ^ ^ @ . . . . . . . .
+. . . . . . @ ^ ^ ^ @ @ @ @ @ . . . . . . . . .
+. . . . . . @ ^ ^ ^ @ . . . . . . . . . . . . .
+. . . . . . @ ^ ^ ^ @ . . . . . . . . . . . . .
+`;
+
+
+
+HUMAN_LIGHT_BASE1   = '#dbb180'   // rgb(219 177 128) - hsl( 32°  56%  68%)
+HUMAN_LIGHT_BASE2   = '#d29d60'   // rgb(210 157  96) - hsl( 32°  56%  60%) - eyes
+HUMAN_LIGHT_BASE3   = '#a66e2c'   // rgb(166 110  44) - hsl( 32°  58%  41%) - eyes
+HUMAN_LIGHT_BASE4   = '#e7cba9'   // rgb(231 203 169) - hsl( 33°  56%  78%)
+HUMAN_LIGHT_BASE5   = '#711010'   // rgb(113  16  16) - hsl(  0°  75%  25%) - mouth
+
+HUMAN_DARKER_BASE1  = '#713f1d'   // rgb(113  63  29) - hsl( 24°  59%  28%)
+HUMAN_DARKER_BASE2  = '#723709'   // rgb(114  55   9) - hsl( 26°  85%  24%) - eyes
+HUMAN_DARKER_BASE3  = '#562600'   // rgb( 86  38   0) - hsl( 27° 100%  17%) - eyes
+HUMAN_DARKER_BASE4  = '#8b532c'   // rgb(139  83  44) - hsl( 25°  52%  36%)
+HUMAN_DARKER_BASE5  = '#4a1201'   // rgb( 74  18   1) - hsl( 14°  97%  15%) - mouth
+
+
+
+
+
+const human_light = Pixelart.parse( human_design, {
+  colors: {
+    '@':  '#000000',       // color 1 - black
+    'x':  HUMAN_LIGHT_BASE3,
+    'o':  HUMAN_LIGHT_BASE2,
+    '^':  HUMAN_LIGHT_BASE1,
+    '~':  HUMAN_LIGHT_BASE4,
+  }} );
+
+
+const human_darker = Pixelart.parse( human_design, {
+  colors: {
+    '@':  '#000000',       // color 1 - black
+    'x':  HUMAN_DARKER_BASE3,
+    'o':  HUMAN_DARKER_BASE2,
+    '^':  HUMAN_DARKER_BASE1,
+    '~':  HUMAN_DARKER_BASE4,
+  }} );
+
+
+
+
 ////////////////////////////////////
 // some add-ons / attributes
 
@@ -105,6 +228,21 @@ const cap = Pixelart.parse( `
      ' x': '#b261dc' },
     offset: [6,4]
    });
+
+
+const knittedcap = Pixelart.parse( `
+  . . . @ @ @ @ @ @ @ . . .
+  . . @ o o o o o o o @ . .
+  . @ o o o o o o o o o @ .
+  @ x x x x x x x x x x x @
+  @ x o x o x o x o x o x @`, {
+    colors: {
+      '@': '#000000',
+      'x': '#933709',
+      'o': '#CA4E11' },
+     offset: [5,5],
+  });
+
 
 
 const headband = Pixelart.parse( `
